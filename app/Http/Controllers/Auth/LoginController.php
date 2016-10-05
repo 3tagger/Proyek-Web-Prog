db82af928b22bdcc4d34f9b44536a529c6acb473
Andri Winata <3tagger95@gmail.com>
2016-10-05 10:39:23 +0700

    Revert "Using username as credential"
    This reverts commit 98ddfc629f628af1bf3f4a507bc1d8d4d28ea42c.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,14 +61,4 @@ class LoginController extends Controller
                 return redirect()->intended($this->redirectPath());
         }
     }
-
-    /**
-     * Get the login username to be used by the controller.
-     *
-     * @return string
-     */
-    public function username()
-    {
-        return 'username';
-    }
 }
